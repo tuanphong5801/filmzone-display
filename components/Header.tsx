56ee665182763589b50e2fab5729be51309dddb3
tuanphong5801 <tuanphong5801@gmail.com>
2023-05-09 15:20:11 +0700

    update searchBtn reponsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -93,7 +93,7 @@ export default function Header() {
               value={searchQuery}
               onChange={handleSearchChange}
               className="bg-gray-800 text-white rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-slate-50 focus:border-transparent"
-              style={{ width: "200px", paddingLeft: "40px", fontSize: "16px" }}
+              style={{ width: "120px", paddingLeft: "40px", fontSize: "16px" }}
             />
             <button type="submit" className="absolute left-3 top-2">
               <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
